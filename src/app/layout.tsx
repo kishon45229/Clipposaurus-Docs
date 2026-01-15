9c0a7be483e0874fea60c33fcc63ffb5f044407c
kishon45229 <kishon45229@outlook.com>
2026-01-15 21:58:48 +0530

    Update icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,9 +48,9 @@ export default function RootLayout({
               hideDefaultCursor={true}
             />
             <OfflineAlert />
-            <div className="flex flex-col min-h-screen md:portrait:min-h-screen md:portrait:justify-center md:portrait:py-10 md:portrait:gap-8 px-1 xs-min:px-2 sm:portrait:px-4 md:portrait:px-8 sm:landscape:px-4 md:landscape:px-6 bg-zinc-50 dark:bg-zinc-950">
+            <div className="flex flex-col">
               <ComponentDataProvider>
-                <div className="sticky top-0 z-10 portrait:h-[14dvh] xs-min:portrait:h-[8dvh] landscape:h-[16dvh] sm:landscape:h-[12dvh] lg:landscape:h-[8dvh] flex-shrink-0 w-full bg-zinc-50/80 dark:bg-zinc-950/80 backdrop-blur-sm border-b border-zinc-200 dark:border-zinc-800 md:portrait:static md:portrait:h-auto md:portrait:bg-transparent md:portrait:backdrop-blur-0 md:portrait:border-b-0">
+                <div className="sticky top-0 z-50 shrink-0 h-[10dvh] w-full bg-zinc-50/80 dark:bg-zinc-950/80 backdrop-blur-sm border-b border-zinc-200 dark:border-zinc-800">
                   <NavbarProvider>
                     <Navbar />
                   </NavbarProvider>
@@ -58,7 +58,7 @@ export default function RootLayout({
                 <div>
                   {children}
                 </div>
-                <div className="portrait:h-[20dvh] xs-min:portrait:h-[8dvh] landscape:h-[16dvh] lg:landscape:h-[8dvh] flex-shrink-0 md:portrait:h-auto md:portrait:flex-none">
+                <div>
                   <FooterProvider>
                     <Footer />
                   </FooterProvider>
